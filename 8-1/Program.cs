cad23758279090cd192ba8d259948ca598bfe9f4
Михаил Семенович <semenovich_misha@mail.ru>
2023-02-16 21:30:18 +0300

    Добавьте файлы проекта.

---
+++ 8-1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+
+namespace DirectoryManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetCatalogs();
+            Count();
+        }
+
+        static void GetCatalogs()
+        {
+
+            string dirName = @"/";
+            if (Directory.Exists(dirName))
+            {
+                Console.WriteLine("Папки: ");
+                string[] dirs = Directory.GetDirectories(dirName);
+
+                foreach (string d in dirs)
+                {
+                    Console.WriteLine(d);
+
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("Файлы: ");
+                string[] files = Directory.GetFiles(dirName);
+
+                foreach (string s in files)
+                    Console.WriteLine(s);
+
+            }
+        }
+        static void Count()
+        {
+
+            try
+            {
+                DirectoryInfo dirInfo = new DirectoryInfo(@"/");
+                if (dirInfo.Exists)
+                {
+                    Console.WriteLine($"Кол-во папок и файлов: {dirInfo.GetDirectories().Length + dirInfo.GetFiles().Length}");
+                }
+                DirectoryInfo newDirectory = new DirectoryInfo(@"/newDirectory");
+                if (!newDirectory.Exists)
+                    newDirectory.Create();
+                Console.WriteLine("Новый директорий создан");
+                DirectoryInfo delInfo = new DirectoryInfo(@"/newDirectory");
+                delInfo.Delete(true);
+                Console.WriteLine("Директорий удален");
+                Console.WriteLine($"Кол-во папок и файлов: {dirInfo.GetDirectories().Length + dirInfo.GetFiles().Length}");
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
